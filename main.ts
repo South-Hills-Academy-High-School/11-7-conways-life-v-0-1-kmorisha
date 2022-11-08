b077f70315f215d9eb01b72139b9a5cd78253bbf
kmorisha <113388413+kmorisha@users.noreply.github.com>
2022-11-08 14:18:26 -0800

    finish the grid

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
+namespace SpriteKind {
+    export const cursor = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    curserGridCol += -1
+    cursorX += -10
+    drawGrid()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    grid[curserGridRow][curserGridCol] = grid[curserGridRow][curserGridCol] * -1 + 1
+    drawGrid()
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    curserGridCol += -1
+    cursorX += -10
+    drawGrid()
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    curserGridCol += 1
+    cursorX += 10
+    drawGrid()
+})
 function drawGrid () {
+    let cursorY = 0
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
     gridSprites = []
     currentY = 0
     for (let row of grid) {
@@ -17,41 +41,52 @@ function drawGrid () {
                     3 3 3 3 3 3 3 3 3 3 
                     3 3 3 3 3 3 3 3 3 3 
                     `, SpriteKind.Player)
-                gridSprite.setPosition(currentX, currentY)
+                gridSprite.left = currentX
+                gridSprite.top = currentY
                 gridSprites.push(gridSprite)
             }
             currentX += 10
         }
         currentY += 10
     }
+    newCursor.left = cursorX
+    newCursor.top = cursorY
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    curserGridCol += 1
+    cursorX += 10
+    drawGrid()
+})
 let gridSprite: Sprite = null
 let currentX = 0
 let currentY = 0
 let gridSprites: Sprite[] = []
+let cursorX = 0
+let curserGridCol = 0
+let curserGridRow = 0
+let newCursor: Sprite = null
 let grid: number[][] = []
-let rowTemplate = [
-0,
-1,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0,
-0
-]
 let row: number[] = []
-for (let index = 0; index < 12; index++) {
-    grid.push(rowTemplate)
+for (let row = 0; row <= 11; row++) {
+    grid.push([])
+    for (let column = 0; column <= 15; column++) {
+        grid[row].push(1)
+    }
 }
-game.onUpdate(function () {
-    drawGrid()
-})
+newCursor = sprites.create(img`
+    9 9 9 . . . . 9 9 9 
+    9 . . . . . . . . 9 
+    9 . . . . . . . . 9 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    9 . . . . . . . . 9 
+    9 . . . . . . . . 9 
+    9 9 9 . . . . 9 9 9 
+    `, SpriteKind.cursor)
+curserGridRow = 0
+curserGridCol = 0
+cursorX = 0
+newCursor.top = 10
+drawGrid()
